7310dc1174392c93ab48429ab2a9b7c72bdca7c7
Himanshu Bafila <himanshubafila@outlook.com>
2020-11-21 16:23:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+
+no = 256
+
+def max_distinct_char(str, n): 
+
+	count = [0] * no  
+	
+	
+	for i in range(n): 
+		count[ord(str[i])] += 1
+	
+	max_distinct = 0
+	for i in range(no): 
+		if (count[i] != 0): 
+			max_distinct += 1	
+	
+	return max_distinct 
+
+def smallesteSubstr_maxDistictChar(str): 
+
+	n = len(str)	 
+	max_distinct = max_distinct_char(str, n) 
+	minl = n	 
+	for i in range(n): 
+		for j in range(n): 
+			subs = str[i:j] 
+			subs_lenght = len(subs) 
+			sub_distinct_char = max_distinct_char(subs, subs_lenght) 
+			if (subs_lenght < minl and
+				max_distinct == sub_distinct_char): 
+				minl = subs_lenght 
+
+	return minl 
+
+
+	
+
+z=input()
+	
+l = smallesteSubstr_maxDistictChar(z); 
+print(l) 
